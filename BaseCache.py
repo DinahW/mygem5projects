63b7467ad50f355e830b87c72780be203597f1e7
DinahW <56730690+DinahW@users.noreply.github.com>
2024-03-18 13:00:00 +0100

    Update BaseCache.py

--- BaseCache.py
+++ BaseCache.py
@@ -8,19 +8,14 @@ class BaseCache(MemObject):
     type = 'BaseCache'
     cxx_header = "mem/cache/base.hh"
     assoc = Param.Int("associativity")
-    hit_latency = Param.Cycles("The hit latency for this cache")
     response_latency = Param.Cycles(
             "Additional cache latency for the return path to core on a miss");
     access_latency = Param.Cycles("Time it takes for the CPU to access the cache/Memory")
     max_miss_count = Param.Counter(0,
         "number of misses to handle before calling exit")
-    mshrs = Param.Int("number of MSHRs (max outstanding requests)")
-    demand_mshr_reserve = Param.Int(1, "mshrs to reserve for demand access")
     size = Param.MemorySize("capacity in bytes")
     forward_snoops = Param.Bool(True,
         "forward snoops from mem side to cpu side")
-    is_top_level = Param.Bool(False, "Is this cache at the top level (e.g. L1)")
-    tgts_per_mshr = Param.Int("max number of accesses per MSHR")
     two_queue = Param.Bool(False,
         "whether the lifo should have two queue replacement")
     write_buffers = Param.Int(8, "number of write buffers")
