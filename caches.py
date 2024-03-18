2b8e8b8a7d5eb14ad12b9ad6bcc7e50f541fae05
DinahW <56730690+DinahW@users.noreply.github.com>
2024-03-18 10:47:53 +0100

    Create caches.py

---
+++ caches.py
@@ -0,0 +1,40 @@
+rom m5.objects import BaseCache
+
+class L1Cache(BaseCache):
+        assoc = 2
+        hit_latency = 2
+        response_latency = 2
+        mshrs = 4
+        tgts_per_mshr = 20
+        is_top_level = True
+
+        def connectCPU(self, cpu):
+            # need to define this in a base class!
+            raise NotImplementedError
+
+        def connectBus(self, bus):
+            self.mem_side = bus.slave
+
+class L1ICache(L1Cache):
+        size = '16kB'
+        def connectCPU(self,cpu):
+            self.cpu_side = cpu.icache_port
+
+class L1DCache(L1Cache):
+        size = '64kB'
+        def connectCPU(self,cpu):
+            self.cpu_side = cpu.dcache_port
+
+class L2Cache(BaseCache):
+        size = '256kB'
+        assoc = 8
+        hit_latency = 20
+        response_latency = 20
+        mshrs = 20
+        tgts_per_mshr = 12
+
+        def connectCPUSideBus(self,bus):
+            self.cpu_side = bus.master
+
+        def connectMemSideBus(self,bus):
+            self.mem_side = bus.slave
